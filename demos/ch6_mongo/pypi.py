bf477ff056b8d62a80dbe6fd2510072f61fa9ffc
Michael Kennedy <mikeckennedy@gmail.com>
2018-04-11 15:55:25 -0700

    mongo final

--- demos/ch6_mongo/pypi.py
+++ demos/ch6_mongo/pypi.py
@@ -6,10 +6,27 @@ from bson import ObjectId
 def main():
     setup.init_mongo()
     add_packages()
+    show_packages()
     print("done.")
 
 
+def show_packages():
+    p = Package.objects(name="mongoengine").first()
+    if not p:
+        print("No package by that name")
+        return
+
+    print(f"Yay found: {p.name}")
+    print("Maintainers: ")
+    for m in p.maintainers:
+        print(m)
+
+
 def add_packages():
+    if Package.objects().count() > 0:
+        print("Already have data")
+        return
+
     p = Package(name="mongoengine")
     p.maintainers.append(ObjectId())
     p.maintainers.append(ObjectId())
